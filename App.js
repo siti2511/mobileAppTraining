f44735155e9dd8312506b3babfff515c753034a5
siti2511 <sitinormas1986@gmail.com>
2021-07-14 13:09:57 +0800

    Perubahan9

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View } from 'react-native';
 import CoursesScreen from './screens/CoursesScreen';
 import HomeSceen from "./screens/HomeSceen";
+import ProfileScreen from './screens/ProfileScreen';
+
 export default function App() {
   return (
     <CoursesScreen/>
